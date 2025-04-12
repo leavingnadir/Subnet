2a7f299292b728f0f2dc3021018102e9c2a8660c
Hirusha Ranaweera <leavingnadir.inc@gmail.com>
2025-04-12 23:38:59 +0530

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-// Move along folks
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function autoSlide() {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}
+
+setInterval(autoSlide, 3000); // change slide every 3 seconds
